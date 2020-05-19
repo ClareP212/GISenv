2f376bf0380c5ffede3e8fa341f06399ff3f1483
ClareP212 <56163311+ClareP212@users.noreply.github.com>
2020-05-19 23:15:20 +0100

    Update ToolTest.py

--- ToolTest.py
+++ ToolTest.py
@@ -11,65 +11,63 @@ from scipy import stats
 import os
 
 # Input parameters
-inputRasters = arcpy.GetParameterAsText(0)
-outputFolder = arcpy.GetParameterAsText(1)
-timeValues = arcpy.GetParameterAsText(2)
+
+
+
+# Input parameters
+outputFolder = arcpy.GetParameterAsText(0)
+inputRasters = arcpy.GetParameterAsText(1)
 
 # Split strings
 inputRasters = inputRasters.split(";")
-timeValues = timeValues.split(";")
 arcpy.AddMessage ("Split parameter texts...")
-#arcpy.AddMessage (timeValues)
-for i in range(len(timeValues)):
-    timeValues[i] = os.path.basename(timeValues[i])
 
-arcpy.env.workspace = "C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0"
-arcpy.env.overwriteOutput = True
-
-# Get raster names from directory
-inputRastName = []
+# Get raster file Directories
+inputRastDir = ['C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2010.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2011.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2012.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2013.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2014.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2015.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2016.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2017.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2018.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2019.tif', 'C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\MOD_an\\2020.tif']
 for i in range(len(inputRasters)):
-    inputRastName.append(os.path.basename(inputRasters[i]))
-  
+    inputRastDir.append(inputRasters[i].split(" ")[0])
+    
+# Get inputted X/time values
 xTime = []
-for i in range(len(inputRastName)):
-    for j in range(len(timeValues)):
-        if (str(inputRastName[i]) in timeValues[j]) == True:
-            #print(inputRastName[i])
-            #print(timeValues[j])
-            xval = timeValues[j].replace(inputRastName[i] + ' ','')
-            xTime.append(float(xval))
+for i in range(len(inputRasters)):
+    xTime.append(os.path.basename(inputRasters[i].split(" ")[1]))
 
-
-noData = 0
+# Get NoData values from input raster 
+for i in inputRastDir:
+    desc=arcpy.Describe(i)
+    #arcpy.AddMessage (desc.noDataValue) 
+    noData = desc.noDataValue
+    
 ## Creating raster stack tif file
-arcpy.CompositeBands_management(inputRasters,"stack.tif")
-stack = "C:\\Users\\Clare\\Documents\\ArcGIS_uni\\GISenv\\0\\stack.tif"
+arcpy.CompositeBands_management(inputRastDir,"stack.tif")
+stack = os.path.join(outputFolder,r"stack.tif")
 stackNP = arcpy.RasterToNumPyArray((arcpy.Raster(stack)),
                          (arcpy.Point((arcpy.Raster(stack)).extent.XMin,
                                       (arcpy.Raster(stack)).extent.YMin)),
                                       (arcpy.Raster(stack)).width,
-                                      (arcpy.Raster(stack)).height,
-                                      0)
-
+                                      (arcpy.Raster(stack)).height)
 arcpy.AddMessage ("Images stacked...")
+
 
 def arcRast (inputRasterList,i):
     """
-    function to convert raster to arcraster
+    function to convert raster to arcraster, returns arc raster
     """
     listItem = inputRasterList[i]
     arcRaster = (arcpy.Raster(listItem))
     return arcRaster
 
 # create empty numpy arrays
-slope_rast = np.zeros((arcRast(inputRasters,1).width,arcRast(inputRasters,1).height)).astype(np.float)
-dataCount_rast = np.zeros((arcRast(inputRasters,1).width,arcRast(inputRasters,1).height))
-pvalue_rast = np.zeros((arcRast(inputRasters,1).width,arcRast(inputRasters,1).height))
+#arcpy.AddMessage ((arcRast(inputRasters,1).height,arcRast(inputRasters,1).width))
+slope_rast = np.zeros((arcRast(inputRastDir,1).height,arcRast(inputRastDir,1).width))
+dataCount_rast = np.zeros((arcRast(inputRastDir,1).height,arcRast(inputRastDir,1).width))
+pvalue_rast = np.zeros((arcRast(inputRastDir,1).height,arcRast(inputRastDir,1).width))
+std_err_rast = np.zeros((arcRast(inputRastDir,1).height,arcRast(inputRastDir,1).width))
+
 
 #Iteration
 row_no = -1
-for row in stackNP[0]:
+for row in slope_rast:
     row_no = row_no +1
     if row_no%100 == 0:
         arcpy.AddMessage ('Row number processed: ' + str(row_no))
@@ -80,9 +78,9 @@ for row in stackNP[0]:
         rem = []
         x = xTime.copy()
         # Stack values for each pixel
-        for i in range(len(inputRasters)):   
+        for i in range(len(inputRastDir)):   
             cell_list.append(int(stackNP[i][row_no][cell_no]))
-            if int(stackNP[i][row_no][cell_no]) == int(0):
+            if int(stackNP[i][row_no][cell_no]) == noData:
                 rem.append(int(i))
 
         # remove noData values
@@ -94,22 +92,24 @@ for row in stackNP[0]:
                 
          # Linear Regression and count datapoints
         if len(cell_list) == 0:
-            #print("No Data")
-            slope_rast[row_no][cell_no] = -9999
+            #arcpy.AddMessage (row_no)
+            #arcpy.AddMessage (cell_no)
+            slope_rast[row_no][cell_no] = noData
             dataCount_rast[row_no][cell_no] = 0
-            pvalue_rast[row_no][cell_no] = -9999
+            pvalue_rast[row_no][cell_no] = noData
         else:
             slope, intercept, r_value, p_value, std_err = stats.linregress(x, cell_list)
             slope_rast[row_no][cell_no] = slope
             pvalue_rast[row_no][cell_no] = p_value
+            std_err_rast[row_no][cell_no] = std_err
             dataCount_rast[row_no][cell_no] = len(cell_list)
 
 ## Output File creation
 # Set Arc environment variables for output
 arcpy.env.overwriteOutput = True
-arcpy.env.outputCoordinateSystem = arcRast(inputRasters,1)
-arcpy.env.cellSize = arcRast(inputRasters,1)
-lowerLeft = arcpy.Point(arcRast(inputRasters,1).extent.XMin,arcRast(inputRasters,1).extent.YMin)
+arcpy.env.outputCoordinateSystem = arcRast(inputRastDir,1)
+arcpy.env.cellSize = arcRast(inputRastDir,1)
+lowerLeft = arcpy.Point(arcRast(inputRastDir,1).extent.XMin,arcRast(inputRastDir,1).extent.YMin)
 
 # Output raster file names and locations
 slopeOut = os.path.join(outputFolder,r"slope.tif")
@@ -118,14 +118,20 @@ pvalueOut = os.path.join(outputFolder,r"pvalue.tif")
 
 #  (bands, rows, columns)
 # change yvalue for band number - i.e. image number
-output_r = arcpy.NumPyArrayToRaster(slope_rast,lowerLeft, arcRast(inputRasters,1).meanCellWidth, arcRast(inputRasters,1).meanCellHeight,noData)
+output_r = arcpy.NumPyArrayToRaster(slope_rast,lowerLeft, arcRast(inputRastDir,1).meanCellWidth, arcRast(inputRastDir,1).meanCellHeight,noData)
 output_r.save(slopeOut)
-output_r = arcpy.NumPyArrayToRaster(dataCount_rast,lowerLeft, arcRast(inputRasters,1).meanCellWidth, arcRast(inputRasters,1).meanCellHeight,noData)
+output_r = arcpy.NumPyArrayToRaster(dataCount_rast,lowerLeft, arcRast(inputRastDir,1).meanCellWidth, arcRast(inputRastDir,1).meanCellHeight,noData)
 output_r.save(dataCountOut)
-output_r = arcpy.NumPyArrayToRaster(pvalue_rast,lowerLeft, arcRast(inputRasters,1).meanCellWidth, arcRast(inputRasters,1).meanCellHeight,noData)
+output_r = arcpy.NumPyArrayToRaster(pvalue_rast,lowerLeft, arcRast(inputRastDir,1).meanCellWidth, arcRast(inputRastDir,1).meanCellHeight,noData)
 output_r.save(pvalueOut)
 
+        
+"""
+Error Handling
+arcpy.AddError(“No ArcMap Documents found. Please check your input \    variables.”) 
 
-            
-
-#arcpy.AddMessage (inputRasters)
+IDEAS
+- ## raster size test
+- irregular shape handling
+- if no data the same
+"""
